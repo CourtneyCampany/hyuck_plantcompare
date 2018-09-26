de37c5dea5cf2988dad14659fb6838bbad7d6b47
Court Campany <ccampany@colgate.edu>
2018-09-26 16:15:24 -0400

    light response data ready to go

--- master_scripts/lightresponse.R
+++ master_scripts/lightresponse.R
@@ -1,4 +1,56 @@
 ##light response
 
 lrc <- read.csv("raw_data/lightresponse.csv")
-species <- read.csv("species_list.csv")
+
+species <- read.csv("raw_data/species_list.csv")
+
+lrc2 <- merge(lrc, species, all=TRUE)
+lightresponse <- lrc2[complete.cases(lrc2$individual),]
+
+
+library(ggplot2)
+angio <- droplevels(lightresponse[lightresponse$plant_group == 'Angiosperm',])
+open <- droplevels(lightresponse[lightresponse$canopy == "Open",])
+closed <- droplevels(lightresponse[lightresponse$canopy == "Closed",])
+
+
+ggplot(data=lightresponse) +
+ geom_smooth(mapping = aes(x=PARi, y=Photo, color=plant_group)) +
+   geom_point(mapping = aes(x=PARi, y=Photo, color=plant_group))
+
+
+
+##appears to be some big differences in angiosperm, is it canopy?
+ggplot(data=angio) +
+  # geom_smooth(mapping = aes(x=PARi, y=Photo, linetype=species)) +
+  geom_point(mapping = aes(x=PARi, y=Photo, color=species)) + theme(plot.subtitle = element_text(vjust = 1), 
+    plot.caption = element_text(vjust = 1), 
+    axis.line = element_line(size = 0.3, 
+        linetype = "solid"), panel.grid.minor = element_line( linetype = "blank"), 
+        panel.background = element_rect(fill = NA, 
+        linetype = "solid")) + theme(panel.grid.minor = element_line(colour = NA))
+
+##looks like just one species
+
+
+##differences within canopy types
+ggplot(data=open) +
+  geom_smooth(mapping = aes(x=PARi, y=Photo, color=plant_group)) +
+  geom_point(mapping = aes(x=PARi, y=Photo, shape=species, color=plant_group))
+
+ggplot(data=closed) +
+  geom_smooth(mapping = aes(x=PARi, y=Photo, color=plant_group)) +
+  geom_point(mapping = aes(x=PARi, y=Photo, shape=species, color=plant_group))
+
+ggplot(data=closed, aes(PARi, Photo)) +
+  geom_smooth(mapping=aes(color=plant_group), span=1) 
+  geom_point(mapping = aes(x=PARi, y=Photo, shape=species, color=plant_group))
+  
+ggplot(data=closed, aes(PARi, Photo)) +
+  geom_point(mapping = aes(color=plant_group))
+
+
+
+
+
+
